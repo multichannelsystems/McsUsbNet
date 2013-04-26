b3c5c94728f19f10dc7b858848cfc85554426db0
Peter Jesinger <jesinger@multichannelsystems.com>
2013-04-26 13:26:36 +0200

    3.1.7

---
+++ get.bat
@@ -0,0 +1,11 @@
+copy \Programming\DllNet\McsUsbNet_v20.xml
+copy \Programming\DllNet\McsUsbNet_v20.dll
+copy \Programming\DllNet\McsUsbUpdateNetDll.dll
+copy \Programming\DllNet\McsUsbUpdateNetDll.XML
+copy \Programming\DllNet\McsUsbNet.xml
+copy \Programming\DllNet\McsUsbNet.dll
+
+copy \Programming\DllNet.x64\McsUsbNet_v20.xml x64
+copy \Programming\DllNet.x64\McsUsbNet_v20.dll x64
+copy \Programming\DllNet.x64\McsUsbNet.xml     x64
+copy \Programming\DllNet.x64\McsUsbNet.dll     x64
